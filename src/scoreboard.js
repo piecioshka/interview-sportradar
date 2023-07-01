7b52a11d77af20ecac0065658e1d79135ba3f6d1
Piotr Kowalski <piecioshka@gmail.com>
2023-07-01 15:28:26 +0200

    Case: Start a match

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,5 +1,19 @@
 const scoreboard = [];
 
+function startMatch(homeName, awayName) {
+  const home = { name: homeName, score: 0 };
+  const away = { name: awayName, score: 0 };
+
+  scoreboard.push({
+    type: 'match',
+    home,
+    away,
+  });
+
+  return { home, away };
+}
+
 module.exports = {
-  scoreboard
+  scoreboard,
+  startMatch,
 };
--- src/scoreboard.spec.js
+++ src/scoreboard.spec.js
@@ -1,7 +1,15 @@
-const { scoreboard } = require('./scoreboard');
+const { scoreboard, startMatch } = require('./scoreboard');
 
 describe('Scoreboard', () => {
   it('should be an empty when any matches are started', () => {
     expect(scoreboard).toEqual([]);
+  });
+
+  it('should start a new match', () => {
+    const match = startMatch('AA', 'BB');
+    expect(match).toEqual({
+      home: { name: 'AA', score: 0 },
+      away: { name: 'BB', score: 0 },
+    });
   });
 });
